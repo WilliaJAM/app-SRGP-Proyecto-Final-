c2d8b960f1b1489acded5aa64db92daff0efb880
WillamJAM <williamjhoanartu@gmail.com>
2024-09-05 07:15:13 -0500

    Restored

--- backend/srgp-app/routes/web.php
+++ backend/srgp-app/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BankController;
-use App\Http\Controllers\UserTableController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
