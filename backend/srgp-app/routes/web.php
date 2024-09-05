202b492746931908cc996a566e96b55aa09cfb26
WillamJAM <williamjhoanartu@gmail.com>
2024-09-05 06:48:58 -0500

    Modified files

--- backend/srgp-app/routes/web.php
+++ backend/srgp-app/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BankController;
-
+use App\Http\Controllers\UserTableController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
